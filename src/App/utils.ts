d81e723ac3b9b7255a0fa49d40530a5498e31134
Frederic Godin <godin.fred8@gmail.com>
2018-07-29 15:55:22 +0200

    Fix getOperation and remove lodash

--- src/App/utils.ts
+++ src/App/utils.ts
@@ -1,5 +1,10 @@
-import getOr = require('lodash/fp/getOr');
 import { OperationDocumentation, MethodResponse } from './types';
+
+const HTTP_METHODS = ['get', 'post', 'put', 'delete'];
+
+function isHttpMethod(value: string): boolean {
+  return HTTP_METHODS.indexOf(value) >= 0;
+}
 
 function getBaseUrl(apiDoc: SwaggerSchema): string {
   if (apiDoc.schemes[0]) {
@@ -8,12 +13,25 @@ function getBaseUrl(apiDoc: SwaggerSchema): string {
   return `${apiDoc.host}${apiDoc.basePath}`;
 }
 
+function getExample(response: OperationResponse): any {
+  if (!response.examples) {
+    return null;
+  }
+  if (!response.examples['application/json']) {
+    return null;
+  }
+  if (!response.examples['application/json'].data) {
+    return null;
+  }
+  return response.examples['application/json'].data;
+}
+
 function formatResponses(docResponses: { [x: string]: OperationResponse }): MethodResponse[] {
   const httpCodes = Object.keys(docResponses);
   return httpCodes.map(code => {
     const docResponse = docResponses[code];
 
-    const example = getOr(null, 'examples.application/json.data', docResponse);
+    const example = getExample(docResponse);
 
     return {
       example,
@@ -30,7 +48,9 @@ function getOperations(apiDoc: SwaggerSchema): OperationDocumentation[] {
   return paths.reduce(
     (operations, path) => {
       const pathDefinition: ApiPath = apiDoc.paths[path] as ApiPath;
-      const pathMethods: HttpMethod[] = Object.keys(pathDefinition) as HttpMethod[];
+      const pathKeys: string[] = Object.keys(pathDefinition) as string[];
+      const pathMethods: HttpMethod[] = pathKeys.filter(isHttpMethod) as HttpMethod[];
+
       const pathOperations: OperationDocumentation[] = pathMethods.map(method => {
         const pathMethod: ApiPathMethod = pathDefinition[method] as ApiPathMethod;
         const responses = formatResponses(pathMethod.responses);
